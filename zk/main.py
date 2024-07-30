03da408c568504225566f643948fbea8a38fbae0
Mischa <94125469+mischavandenburg@users.noreply.github.com>
2024-07-30 08:52:49 +0200

    feat!: remove custom errors

--- zk/main.py
+++ zk/main.py
@@ -2,7 +2,6 @@ import os
 import re
 import subprocess
 from typing import Optional
-
 import typer
 from rich import print
 from typing_extensions import Annotated
@@ -11,24 +10,6 @@ from typing_extensions import Annotated
 MAX_TITLE_LENGTH = 80
 INBOX_PATH = "/Users/mischa/Zettelkasten/0 Inbox"
 PROMPT_TITLE = "Enter the title of the note"
-
-
-# Custom Exceptions
-class EmptyTitleError(ValueError):
-    """Raised when the note title is empty."""
-
-
-class TitleTooLongError(ValueError):
-    """Raised when the note title exceeds the maximum length."""
-
-
-class FileExistsError(IOError):
-    """Raised when trying to create a file that already exists."""
-
-
-class InvalidInputError(ValueError):
-    """Raised when the input doesn't match the expected format."""
-
 
 app = typer.Typer()
 
@@ -41,7 +22,10 @@ def new(title: Annotated[Optional[str], typer.Argument()] = None) -> None:
         validate_title(note_title)
         file_path = format_path(note_title)
         create_and_open_file(file_path, note_title)
-    except (EmptyTitleError, TitleTooLongError, FileExistsError) as e:
+    except ValueError as e:
+        typer.echo(f"Error: {str(e)}", err=True)
+        raise typer.Exit(code=1)
+    except FileExistsError as e:
         typer.echo(f"Error: {str(e)}", err=True)
         raise typer.Exit(code=1)
     except Exception as e:
@@ -57,11 +41,9 @@ def get_note_title(title: Optional[str]) -> str:
 def validate_title(title: str) -> None:
     """Validate the note title."""
     if not title:
-        raise EmptyTitleError("Note title cannot be empty.")
+        raise ValueError("Note title cannot be empty.")
     if len(title) > MAX_TITLE_LENGTH:
-        raise TitleTooLongError(
-            f"Title cannot be more than {MAX_TITLE_LENGTH} characters."
-        )
+        raise ValueError(f"Title cannot be more than {MAX_TITLE_LENGTH} characters.")
     if title.endswith(".md"):
         raise ValueError("Leave out the .md extension.")
 
@@ -93,22 +75,22 @@ def create_and_open_file(file_path: str, note_title: str) -> None:
     """Create a new note file and open it in Neovim."""
     if os.path.exists(file_path):
         raise FileExistsError(f"The file already exists: {file_path}")
-
     create_note_file(file_path, note_title)
     print(f"New note created: {file_path}")
     open_in_neovim(file_path)
 
 
-# To allow input like "- [[this is my new note]]", we need to allow extra args
 @app.command(
     context_settings={"allow_extra_args": True, "ignore_unknown_options": True}
 )
 def new_note_from_vim(context: typer.Context) -> None:
     """Create new note and add link to daily note."""
-
-    print(context.args)
-    file_name = format_from_vim(" ".join(context.args))
-    print(file_name)
+    try:
+        file_name = format_from_vim(" ".join(context.args))
+        print(file_name)
+        # TODO: Implement the logic to add the link to the daily note
+    except ValueError as e:
+        print(f"Error: {str(e)}")
 
 
 def format_from_vim(input_line: str) -> str:
@@ -122,9 +104,9 @@ def format_from_vim(input_line: str) -> str:
         str: The extracted file name with '.md' extension.
 
     Raises:
-        InvalidInputError: If no text is found between double brackets.
+        ValueError: If no text is found between double brackets.
     """
     match = re.search(r"\[\[(.*?)\]\]", input_line)
     if not match:
-        raise InvalidInputError("No text found between double brackets")
+        raise ValueError("No text found between double brackets")
     return f"{match.group(1)}.md"
